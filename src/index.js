8f7e1467ea2d918dc9998b7952182c82f78fd6d6
Mohamed Shiyas B <147586484+Shiyas9961@users.noreply.github.com>
2024-03-02 23:48:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,14 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route 
             path='/*'
             element={<App/>}
           />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   //</React.StrictMode>
 );
