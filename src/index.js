adb88c84fe0bf92d6c07164fc272debff2b81581
Mohamed Shiyas B <147586484+Shiyas9961@users.noreply.github.com>
2024-03-02 20:01:24 +0530

    Update index.js
    added hash router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,14 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route 
             path='/*'
             element={<App/>}
           />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   //</React.StrictMode>
 );
